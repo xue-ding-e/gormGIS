5ce0e30dff1cda2a0e3ba3aa49447deb21acdced
Qing Liang <106448173+xue-ding-e@users.noreply.github.com>
2025-09-05 10:45:16 +0800

    feat:default; process nil

--- geopoint.go
+++ geopoint.go
@@ -10,19 +10,62 @@ import (
 )
 
 type GeoPoint struct {
-	Lng float64 `json:"lng"`
-	Lat float64 `json:"lat"`
+	Lng  float64 `json:"lng"`
+	Lat  float64 `json:"lat"`
+	SRID int     `json:"srid,omitempty"` // default 4326
+}
+
+func (p *GeoPoint) SetSRID(srid int) *GeoPoint {
+	p.SRID = srid
+	return p
+}
+
+func NewGeoPoint(lng, lat float64) *GeoPoint {
+	return &GeoPoint{
+		Lng:  lng,
+		Lat:  lat,
+		SRID: 4326,
+	}
+}
+
+func NewGeoPointWithSRID(lng, lat float64, srid int) *GeoPoint {
+	return &GeoPoint{
+		Lng:  lng,
+		Lat:  lat,
+		SRID: srid,
+	}
 }
 
 func (p *GeoPoint) String() string {
-	return fmt.Sprintf("SRID=4326;POINT(%v %v)", p.Lng, p.Lat)
+	srid := p.SRID
+	if srid == 0 {
+		srid = 4326
+	}
+	return fmt.Sprintf("SRID=%d;POINT(%v %v)", srid, p.Lng, p.Lat)
 }
 
 func (p *GeoPoint) Scan(val interface{}) error {
-	b, err := hex.DecodeString(string(val.([]uint8)))
+	if val == nil {
+		return nil
+	}
+
+	var b []byte
+	var err error
+
+	// 处理不同类型的输入
+	switch v := val.(type) {
+	case []uint8:
+		b, err = hex.DecodeString(string(v))
+	case string:
+		b, err = hex.DecodeString(v)
+	default:
+		return fmt.Errorf("cannot scan %T into GeoPoint", val)
+	}
+
 	if err != nil {
 		return err
 	}
+
 	r := bytes.NewReader(b)
 	var wkbByteOrder uint8
 	if err := binary.Read(r, binary.LittleEndian, &wkbByteOrder); err != nil {
